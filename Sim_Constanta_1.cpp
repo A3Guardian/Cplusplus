6e51cb298a9f3d4a77148006e6b5c0ab98a85f72
A3Guardian <astefanoaieduard@gmail.com>
2023-02-12 21:32:00 +0200

    start sim constanta

---
+++ Sim_Constanta_1.cpp
@@ -0,0 +1,28 @@
+//Subiectul 1
+//1.b
+//2.d
+//3.b
+//4.a
+//5.d
+
+//Subiectul 2
+//1.
+
+//2.
+
+//3.
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n, M[25][25];
+//     cin>>n;
+//     for(int i=1;i<=n;i++)
+//       for(int j=1;j<=n;j++)
+//          M[i][j]=i*j%10;
+
+//     for(int i=1;i<=n;i++)
+//     cout<<endl;
+//       for(int j=1;j<=n;j++)
+//          cout<<M[i][j]<<' ';
+
+// }
